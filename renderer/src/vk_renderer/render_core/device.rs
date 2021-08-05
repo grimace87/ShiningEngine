54136ee5fc7c2c988447df8cc570728c2ba5d21b
Thomas <thomasr1987@gmail.com>
2021-08-06 07:31:50 +1000

    Put device selection and allocation in a separate file

---
+++ renderer/src/vk_renderer/render_core/device.rs
@@ -0,0 +1,116 @@
+
+use ash::{
+    vk,
+    Instance,
+    Device,
+    extensions::khr::{Surface, Swapchain},
+    version::{InstanceV1_0, DeviceV1_0}
+};
+use std::os::raw::c_char;
+
+#[derive(Copy, Clone)]
+pub struct PhysicalDeviceProperties {
+    pub physical_device: vk::PhysicalDevice,
+    pub graphics_queue_family_index: u32,
+    pub transfer_queue_family_index: u32,
+}
+
+pub unsafe fn make_device_resources(instance: &Instance, surface_fn: &Surface, surface: &vk::SurfaceKHR) -> Result<(Device, PhysicalDeviceProperties, vk::Queue, vk::Queue), String> {
+
+    let physical_device_properties = select_physical_device(&instance, surface_fn, surface)?;
+
+    // Find queue indices for graphics and transfer (ideally different but could be the same)
+    let queue_family_properties = instance
+        .get_physical_device_queue_family_properties(physical_device_properties.physical_device);
+    let (graphics_queue_family_index, transfer_queue_family_index) = {
+        let mut found_graphics_queue_index = None;
+        let mut found_transfer_queue_index = None;
+        for (index, queue_family) in queue_family_properties.iter().enumerate() {
+            if queue_family.queue_count > 0 && queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
+                found_graphics_queue_index = Some(index as u32);
+            }
+            if queue_family.queue_count > 0 && queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER) {
+                if found_transfer_queue_index.is_none() || !queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
+                    found_transfer_queue_index = Some(index as u32);
+                }
+            }
+        }
+        (
+            found_graphics_queue_index.unwrap(),
+            found_transfer_queue_index.unwrap()
+        )
+    };
+
+    // Device extensions required
+    let device_extensions: Vec<*const c_char> = vec![ Swapchain::name().as_ptr() ];
+
+    // Make the logical device
+    let priorities = [1.0f32];
+    let queue_infos = [
+        vk::DeviceQueueCreateInfo::builder()
+            .queue_family_index(graphics_queue_family_index)
+            .queue_priorities(&priorities)
+            .build(),
+        vk::DeviceQueueCreateInfo::builder()
+            .queue_family_index(transfer_queue_family_index)
+            .queue_priorities(&priorities)
+            .build()
+    ];
+    let device_create_info = vk::DeviceCreateInfo::builder()
+        .queue_create_infos(&queue_infos)
+        .enabled_extension_names(&device_extensions);
+    let device = instance
+        .create_device(physical_device_properties.physical_device, &device_create_info, None)
+        .map_err(|e| format!("{:?}", e))?;
+
+    // Get queues
+    let graphics_queue = device.get_device_queue(graphics_queue_family_index, 0);
+    let transfer_queue = device.get_device_queue(transfer_queue_family_index, 0);
+
+    Ok((device, physical_device_properties, graphics_queue, transfer_queue))
+}
+
+unsafe fn select_physical_device(
+    instance: &ash::Instance,
+    surface_loader: &ash::extensions:: khr::Surface,
+    surface: &vk::SurfaceKHR
+) -> Result<PhysicalDeviceProperties, String> {
+
+    let physical_devices = instance
+        .enumerate_physical_devices()
+        .map_err(|e| format!("{:?}", e))?;
+    if physical_devices.is_empty() {
+        return Err(String::from("No physical devices found"));
+    }
+
+    let unset_value: u32 = u32::MAX;
+    for physical_device in physical_devices.iter() {
+        let queue_family_properties = instance.get_physical_device_queue_family_properties(*physical_device);
+        let mut graphics_index: u32 = unset_value;
+        let mut transfer_index: u32 = unset_value;
+        for (index, properties) in queue_family_properties.iter().enumerate() {
+            let supports_graphics = properties.queue_flags.contains(vk::QueueFlags::GRAPHICS);
+            let supports_surface = surface_loader
+                .get_physical_device_surface_support(*physical_device, index as u32, *surface)
+                .unwrap();
+            let supports_transfer = properties.queue_flags.contains(vk::QueueFlags::TRANSFER);
+
+            let graphics_and_surface = supports_graphics && supports_surface;
+            if graphics_and_surface {
+                graphics_index = index as u32;
+            }
+            if supports_transfer && (transfer_index == unset_value || !graphics_and_surface) {
+                transfer_index = index as u32;
+            }
+        }
+        if graphics_index != unset_value && transfer_index != unset_value {
+            return Ok(PhysicalDeviceProperties {
+                physical_device: *physical_device,
+                graphics_queue_family_index: graphics_index,
+                transfer_queue_family_index: transfer_index
+            });
+        }
+    }
+
+    Err(String::from("Could not find a suitable physical device"))
+}
--- renderer/src/vk_renderer/render_core/mod.rs
+++ renderer/src/vk_renderer/render_core/mod.rs
@@ -1,17 +1,15 @@
 
 mod instance;
 mod debug;
+mod device;
 
 use crate::vk_renderer::images::ImageWrapper;
 use instance::make_instance;
 use debug::make_debug_utils;
+use device::{PhysicalDeviceProperties, make_device_resources};
 
 use defs::{PresentResult, ResourcePreloads, VertexFormat, ImageUsage, TexturePixelFormat};
 
-use std::{
-    ffi::CString,
-    os::raw::c_char
-};
 use ash::{
     vk,
     Entry,
@@ -30,13 +28,6 @@ use crate::vk_renderer::buffers::BufferWrapper;
 
 pub const PROJ_VK_SWAPCHAIN_SIZE: usize = 2;
 
-#[derive(Copy, Clone)]
-pub struct PhysicalDeviceProperties {
-    pub physical_device: vk::PhysicalDevice,
-    pub graphics_queue_family_index: u32,
-    pub transfer_queue_family_index: u32,
-}
-
 /// Wraps resources that either never change while the app is running, or rarely change (perhaps
 /// when a window is resized or the surface becomes invalidated for some other reason).
 ///
@@ -49,9 +40,9 @@ pub struct RenderCore {
     debug_utils: Option<(DebugUtils, vk::DebugUtilsMessengerEXT)>,
     pub device: Device,
     pub physical_device_properties: PhysicalDeviceProperties,
-    mem_allocator: vk_mem::Allocator,
     pub graphics_queue: vk::Queue,
     pub transfer_queue: vk::Queue,
+    mem_allocator: vk_mem::Allocator,
     pub graphics_command_buffer_pool: vk::CommandPool,
     pub transfer_command_buffer_pool: vk::CommandPool,
     sync_image_available: Vec<vk::Semaphore>,
@@ -108,77 +99,20 @@ impl RenderCore {
 
     unsafe fn new_with_surface_without_swapchain(entry: &Entry, window_owner: &dyn HasRawWindowHandle) -> Result<RenderCore, String> {
 
+        // Create instance
         let instance = make_instance(entry, window_owner)?;
         let debug_utils = make_debug_utils(entry, &instance)?;
 
         // Create surface and surface loader, and chosoe a compatible physical device
         let surface_fn = Surface::new(entry, &instance);
         let surface = RenderCore::make_new_surface(entry, &instance, window_owner);
-        let physical_device_properties = RenderCore::select_physical_device(&instance, &surface_fn, &surface)?;
-        let physical_device = physical_device_properties.physical_device;
 
-        // Find queue indices for graphics and transfer (ideally different but could be the same)
-        let queue_family_properties = instance
-            .get_physical_device_queue_family_properties(physical_device);
-        let (graphics_queue_family_index, transfer_queue_family_index) = {
-            let mut found_graphics_queue_index = None;
-            let mut found_transfer_queue_index = None;
-            for (index, queue_family) in queue_family_properties.iter().enumerate() {
-                if queue_family.queue_count > 0 && queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
-                    found_graphics_queue_index = Some(index as u32);
-                }
-                if queue_family.queue_count > 0 && queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER) {
-                    if found_transfer_queue_index.is_none() || !queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
-                        found_transfer_queue_index = Some(index as u32);
-                    }
-                }
-            }
-            (
-                found_graphics_queue_index.unwrap(),
-                found_transfer_queue_index.unwrap()
-            )
-        };
-
-        // Device extensions required
-        let device_extensions: Vec<*const c_char> = vec![ Swapchain::name().as_ptr() ];
-
-        // NOTE: Device validation layers are deprecated and usually the layers enabled on the
-        // instance will cover all API calls
-        let layer_names: Vec<CString> = vec![
-            CString::new("VK_LAYER_KHRONOS_validation").unwrap()
-        ];
-        let layer_name_pointers: Vec<*const c_char> = layer_names
-            .iter()
-            .map(|layer_name| layer_name.as_ptr())
-            .collect();
-
-        // Make the logical device
-        let priorities = [1.0f32];
-        let queue_infos = [
-            vk::DeviceQueueCreateInfo::builder()
-                .queue_family_index(graphics_queue_family_index)
-                .queue_priorities(&priorities)
-                .build(),
-            vk::DeviceQueueCreateInfo::builder()
-                .queue_family_index(transfer_queue_family_index)
-                .queue_priorities(&priorities)
-                .build()
-        ];
-        let device_create_info = vk::DeviceCreateInfo::builder()
-            .queue_create_infos(&queue_infos)
-            .enabled_layer_names(&layer_name_pointers)
-            .enabled_extension_names(&device_extensions);
-        let device = instance
-            .create_device(physical_device, &device_create_info, None)
-            .map_err(|e| format!("{:?}", e))?;
-
-        // Get queues
-        let graphics_queue = device.get_device_queue(graphics_queue_family_index, 0);
-        let transfer_queue = device.get_device_queue(transfer_queue_family_index, 0);
+        // Create device and queues
+        let (device, physical_device_properties, graphics_queue, transfer_queue) = make_device_resources(&instance, &surface_fn, &surface)?;
 
         // Create a memory allocator
         let allocator_info = vk_mem::AllocatorCreateInfo {
-            physical_device,
+            physical_device: physical_device_properties.physical_device,
             device: device.clone(),
             instance: instance.clone(),
             flags: AllocatorCreateFlags::NONE,
@@ -191,13 +125,13 @@ impl RenderCore {
 
         // One command buffer pool per queue family
         let graphics_pool_info = vk::CommandPoolCreateInfo::builder()
-            .queue_family_index(graphics_queue_family_index)
+            .queue_family_index(physical_device_properties.graphics_queue_family_index)
             .flags(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER);
         let graphics_command_buffer_pool = device
             .create_command_pool(&graphics_pool_info, None)
             .map_err(|e| format!("{:?}", e))?;
         let transfer_pool_info = vk::CommandPoolCreateInfo::builder()
-            .queue_family_index(transfer_queue_family_index)
+            .queue_family_index(physical_device_properties.transfer_queue_family_index)
             .flags(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER);
         let transfer_command_buffer_pool = device
             .create_command_pool(&transfer_pool_info, None)
@@ -233,9 +167,9 @@ impl RenderCore {
                 debug_utils,
                 device,
                 physical_device_properties,
-                mem_allocator,
                 graphics_queue,
                 transfer_queue,
+                mem_allocator,
                 graphics_command_buffer_pool,
                 transfer_command_buffer_pool,
                 sync_image_available,
@@ -295,51 +229,6 @@ impl RenderCore {
         }
 
         Ok(())
-    }
-
-    unsafe fn select_physical_device(
-            instance: &ash::Instance,
-            surface_loader: &ash::extensions:: khr::Surface,
-            surface: &vk::SurfaceKHR
-            ) -> Result<PhysicalDeviceProperties, String> {
-
-        let physical_devices = instance
-            .enumerate_physical_devices()
-            .map_err(|e| format!("{:?}", e))?;
-        if physical_devices.is_empty() {
-            return Err(String::from("No physical devices found"));
-        }
-
-        let unset_value: u32 = u32::MAX;
-        for physical_device in physical_devices.iter() {
-            let queue_family_properties = instance.get_physical_device_queue_family_properties(*physical_device);
-            let mut graphics_index: u32 = unset_value;
-            let mut transfer_index: u32 = unset_value;
-            for (index, properties) in queue_family_properties.iter().enumerate() {
-                let supports_graphics = properties.queue_flags.contains(vk::QueueFlags::GRAPHICS);
-                let supports_surface = surface_loader
-                    .get_physical_device_surface_support(*physical_device, index as u32, *surface)
-                    .unwrap();
-                let supports_transfer = properties.queue_flags.contains(vk::QueueFlags::TRANSFER);
-
-                let graphics_and_surface = supports_graphics && supports_surface;
-                if graphics_and_surface {
-                    graphics_index = index as u32;
-                }
-                if supports_transfer && (transfer_index == unset_value || !graphics_and_surface) {
-                    transfer_index = index as u32;
-                }
-            }
-            if graphics_index != unset_value && transfer_index != unset_value {
-                return Ok(PhysicalDeviceProperties {
-                    physical_device: *physical_device,
-                    graphics_queue_family_index: graphics_index,
-                    transfer_queue_family_index: transfer_index
-                });
-            }
-        }
-
-        Err(String::from("Could not find a suitable physical device"))
     }
 
     unsafe fn destroy_all_resources(&mut self) {
